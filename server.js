e315dda1e72019e4c95e17214b8574c111795c6b
Danylo <warfais6@gmail.com>
2023-12-14 00:24:16 +0200

    Rework Project

--- server.js
+++ server.js
@@ -3,64 +3,69 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
-  bodyParser = require("body-parser"),
-  swaggerJsdoc = require("swagger-jsdoc"),
-  swaggerUi = require("swagger-ui-express");
-  
+  bodyParser = require('body-parser'),
+  swaggerJsdoc = require('swagger-jsdoc'),
+  swaggerUi = require('swagger-ui-express')
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.Promise = global.Promise
+mongoose.connect(
+  'mongodb+srv://warfais6:Qwekvtr2112@cluster0.5dz6l4u.mongodb.net/localhost'
+)
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+var routes = require('./api/routes/todoListRoutes')
+routes(app)
 
 const options = {
   definition: {
-    openapi: "3.1.0",
+    openapi: '3.1.0',
     info: {
-      title: "LogRocket Express API with Swagger",
-      version: "0.1.0",
+      title: 'LogRocket Express API with Swagger',
+      version: '0.1.0',
       description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
+        'This is a simple CRUD API application made with Express and documented with Swagger',
       license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html'
       },
       contact: {
-        name: "LogRocket",
-        url: "https://logrocket.com",
-        email: "info@email.com",
-      },
+        name: 'LogRocket',
+        url: 'https://logrocket.com',
+        email: 'info@email.com'
+      }
     },
     servers: [
       {
-        url: "http://localhost:3000",
-      },
-    ],
+        url: 'http://localhost:3000'
+      }
+    ]
   },
-  apis: ["./api/routes/*.js"],
-};
+  apis: ['./api/routes/*.js']
+}
 
-const specs = swaggerJsdoc(options);
+const specs = swaggerJsdoc(options)
 app.use(
-  "/api-docs",
+  '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(specs, {
     explorer: true,
     customCssUrl:
-      "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+      'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css'
   })
-);
-  
+)
 
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+})
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
+var routes = require('./api/routes/todoListRoutes')
+routes(app)
 
-var routes = require('./api/routes/todoListRoutes');
-routes(app); 
-
-
-app.listen(port);
-
-
-console.log('todo list RESTful API server started on: ' + port);
+app.listen(port, () => {
+  console.log(`Server listen ` + port)
+})
